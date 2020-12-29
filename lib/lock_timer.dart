193e27dcf2fa98e69a2c7a6d452932cc59094263
Yuqian Li <liyuqian@google.com>
2020-12-29 14:10:25 -0800

    Add Linux support

--- lib/lock_timer.dart
+++ lib/lock_timer.dart
@@ -17,18 +17,23 @@ class LockTimer extends SinglePositionalArgCommand {
   @override
   FutureOr<void> runWithSingleArg(String arg) async {
     final minutes = int.parse(arg);
-    if (minutes < 3) {
-      throw 'We only support a timeout of at least 3 minutes.';
+    if (minutes > 3) {
+      final duration = Duration(minutes: minutes);
+      final durationMinus3 = duration - Duration(minutes: 3);
+      await Future<void>.delayed(durationMinus3);
     }
-    final duration = Duration(minutes: int.parse(arg));
-    final durationMinus3 = duration - Duration(minutes: 3);
-    await Future<void>.delayed(durationMinus3);
-    await _beep();
-    await _seconds(60);
-    await _beep(2);
-    await _seconds(60);
-    await _beep(3);
-    await _seconds(50);
+    if (minutes >= 3) {
+      await _beep();
+      await _seconds(60);
+    }
+    if (minutes >= 2) {
+      await _beep(2);
+      await _seconds(60);
+    }
+    if (minutes >= 1) {
+      await _beep(3);
+      await _seconds(50);
+    }
     for (int i = 0; i < 10; i += 1) {
       print('Countdown ${10 - i}');
       await _beep();
@@ -39,6 +44,9 @@ class LockTimer extends SinglePositionalArgCommand {
       result = Process.runSync('pmset', ['displaysleepnow']);
     } else if (Platform.isWindows) {
       result = Process.runSync('Rundll32.exe', ['user32.dll,LockWorkStation']);
+    } else if (Platform.isLinux) {
+      // `sudo apt install gnome-screensaver` may be needed
+      result = Process.runSync('gnome-screensaver-command', ['-l']);
     } else {
       throw 'We don\'t support screenlock in this platform yet.';
     }
@@ -53,6 +61,8 @@ class LockTimer extends SinglePositionalArgCommand {
 
   Future<void> _beep([int times = 1]) async {
     for (int i = 0; i < times; i += 1) {
+      // In case there are many audio output interfaces, make sure to select the
+      // one that can make sounds. Ubuntu/Linux seems to be bad at this.
       print('beep' + String.fromCharCodes([0x07]));
       await Future<void>.delayed(Duration(milliseconds: 200));
     }
